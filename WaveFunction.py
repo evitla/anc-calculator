505909950b2e2459456ad605770339eaadd3fbe0
Dinmukhamed Sailaubek <s.dinmuhamed@gmail.com>
2020-07-22 14:26:53 +0600

    Fixed error in WaveFunction().N_cos

--- WaveFunction.py
+++ WaveFunction.py
@@ -10,7 +10,7 @@ class WaveFunction:
         self.eta = alpha * (1 + 1j * b)
         l15 = self.l + 1.5
         b_sq1 = (b**2 + 1)**(l15/2)
-        self.N_cos = 2 * (2 * alpha)**(l15/2) * b_sq1 / np.sqrt(gamma(l15) * (b_sq1 + np.cos(l15 * np.arctan(b))))
+        self.N_cos = 2 * (2 * alpha)**(l15/2) * np.sqrt(b_sq1 / gamma(l15) / (b_sq1 + np.cos(l15 * np.arctan(b))))
 
     def psi(self, r):
         return np.einsum("i,ir->ir", self.N_cos / 2, r**self.l * np.exp(-np.multiply.outer(self.eta, r**2)))
@@ -20,28 +20,29 @@ class WaveFunction:
 
     def draw_phi(self, r, *j, f="wavefunctions"):
         fig, ax = plt.subplots(figsize=(10, 6))
-        if f == "density":
-            p, q = j
-            plt.plot(r, self.phi(r)[p] * self.phi(r)[q], lw=2)
-            ylab = f"$\phi_{p}^*\phi_{q}$"
-        else:
-            [plt.plot(r, self.phi(r)[i], lw=2, label=f"$\phi_{i}$") for i in j]
-            ylab = "$\phi_i$"
         font = {
             "family": "Arial",
             "color": "black",
             "weight": "normal",
             "size": 14
         }
+        if f == "density":
+            p, q = j
+            plt.plot(r, self.phi(r)[p] * self.phi(r)[q], lw=2)
+            ylab = f"$\phi_{p}^*\phi_{q}$"
+        else:
+            [plt.plot(r, self.phi(r)[i], lw=2, label=f"$\phi_{i}$") for i in j]
+            plt.legend(fontsize=font["size"] - 2)
+            ylab = "$\phi_i$"
         ax.tick_params(axis="both", labelsize=font["size"] - 2)
         plt.xlabel("$r$, fm", fontdict=font)
         plt.ylabel(ylab, fontdict=font)
-        plt.legend(fontsize=font["size"] - 2)
         plt.show()
 
 
-wf_params = [1, 10, 1, 0.1, np.pi / 4]
-wf = WaveFunction(wf_params)
-r = np.arange(0, 20, 0.1)
-wf.draw_phi(r, *range(5))
-wf.draw_phi(r, 1, 5, f="density")
+if __name__ == "__main__":
+    wf_params = [1, 10, 1, 0.1, np.pi / 4]
+    wf = WaveFunction(wf_params)
+    r = np.arange(0, 20, 0.1)
+    wf.draw_phi(r, *range(5))
+    wf.draw_phi(r, 1, 5, f="density")
